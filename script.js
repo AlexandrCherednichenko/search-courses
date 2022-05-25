ef69e2d44850de7a1b5c1c430948035e5b868a7a
AlexanderCherednichenko <>
2022-05-25 12:38:30 +0300

    writing the main logic for searching

--- script.js
+++ script.js
@@ -11,9 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
       { name: "Courses in Kazakhstan", prices: [56, 324] },
       { name: "Courses in France", prices: [null, null] },
    ];
+   let arrValue = [];
+   let arr1 = [];
+   let arr2 = [];
 
    let coursesBlock = document.querySelector('.courses');
-   let coursesItem
+   let item
 
    let priceFrom = document.querySelector('.price__input-from input');
    let priceTo = document.querySelector('.price__input-to input');
@@ -22,39 +25,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
    let clearFilter = document.querySelector('.filter__clear');
 
-   // функция для ввода только чисел 
-   function inputNumber(input) {
-      // проверка на пробелы 
-      if (input.value == false) {
-         input.value = "";
-      }
-
-      if (input.value.length > 0) {
-         let inputDataArr = input.value.split(/[- — /]/);
-         let inputDataClear = inputDataArr.join('');
-         let inputDataArrNew = inputDataClear.match(/.{1,1}/g);
-
-         // отсекает первый ноль если больше 1 символа
-         if (inputDataArrNew[0] == 0 && inputDataArrNew.length == 2) {
-            inputDataArrNew.shift();
-         }
-
-         let inputDataArrClear = inputDataArrNew.filter(function (arr) {
-            return arr.match(/^[1-9]|[0-9]|[0-9]$/g);
-         });
-
-         // фильтр на посторонние символы
-         if (!inputDataArrNew[inputDataArrNew.length - 1].match(/[0-9]/)) {
-            inputDataArrNew.length = inputDataArrNew.length - 1
-            input.value = inputDataArrNew.join('');
-         }
-         input.value = inputDataArrClear.join('');
-      }
-   }
 
    // ввод значений 
    function inputPrice(inputName) {
       inputName.addEventListener('input', () => {
+         // функция для ввода только чисел 
+         function inputNumber(input) {
+            // проверка на пробелы 
+            if (input.value == false) {
+               input.value = "";
+            }
+
+            if (input.value.length > 0) {
+               let inputDataArr = input.value.split(/[- — /]/);
+               let inputDataClear = inputDataArr.join('');
+               let inputDataArrNew = inputDataClear.match(/.{1,1}/g);
+
+               // отсекает первый ноль если больше 1 символа
+               if (inputDataArrNew[0] == 0 && inputDataArrNew.length == 2) {
+                  inputDataArrNew.shift();
+               }
+
+               let inputDataArrClear = inputDataArrNew.filter(function (arr) {
+                  return arr.match(/^[1-9]|[0-9]|[0-9]$/g);
+               });
+
+               // фильтр на посторонние символы
+               if (!inputDataArrNew[inputDataArrNew.length - 1].match(/[0-9]/)) {
+                  inputDataArrNew.length = inputDataArrNew.length - 1
+                  input.value = inputDataArrNew.join('');
+               }
+               input.value = inputDataArrClear.join('');
+            }
+         }
          inputNumber(inputName)
 
          if (inputName.value.length > 0) {
@@ -64,6 +67,8 @@ document.addEventListener("DOMContentLoaded", function () {
          }
       })
    }
+   priceFrom.onfocus = inputPrice(priceFrom)
+   priceTo.onfocus = inputPrice(priceTo)
 
    // очистить инпуты
    function clearInput(inputName) {
@@ -72,27 +77,22 @@ document.addEventListener("DOMContentLoaded", function () {
    }
 
    // создание списка 
-   function createList() {
+   function createList(arr) {
       coursesBlock.textContent = '';
-      courses.forEach(elem => {
-         coursesItem = `<li>${elem['name']} [${elem['prices']}]</li>`
-         coursesBlock.insertAdjacentHTML("beforeend", coursesItem)
+      arr.forEach(elem => {
+         item =
+            `<li>
+               <span class="name">${elem['name']}.</span>
+               <span>Prices from: ${elem['prices'][0] != null ? elem['prices'][0] : 'price from not specified'}.</span>
+               <span>Prices to: ${elem['prices'][1] != null ? elem['prices'][1] : 'price to not specified'}.</span>
+            </li>`
+         coursesBlock.insertAdjacentHTML("beforeend", item)
       })
    }
-   createList()
+   createList(courses)
 
    document.addEventListener('click', (event) => {
       let target = event.target;
-
-      // ввод значения цена от
-      if (target == priceFrom) {
-         inputPrice(priceFrom)
-      }
-
-      // ввод значения цена до
-      if (target == priceTo) {
-         inputPrice(priceTo)
-      }
 
       // нажатие на крестик в поле от
       if (target.closest('.clear') && target.closest('.price__input-from')) {
@@ -106,27 +106,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // нажатие кнопк поиск
       if (target == serchBtn) {
-         let valueFrom = Number(priceFrom.value)
-         let valueTo = Number(priceTo.value)
+         let valueFrom = priceFrom.value
+         let valueTo = priceTo.value
 
-         courses.forEach(elem => {
+         courses.forEach((elem, index) => {
             if (valueFrom != 0 && valueTo != 0) {
-               if (valueFrom >= elem['prices'][0] && valueFrom <= elem['prices'][1]) {
-                  console.log(elem);
+               if ((valueFrom <= elem['prices'][1] || elem['prices'][1] == null) && (valueTo >= elem['prices'][0] || elem['prices'][0] == null)) {
+                  arrValue.push(elem)
                }
-            } else if (valueFrom != 0 && valueTo == 0) {
-            } else if (valueFrom == 0 && valueTo != 0) {
-               // if (valueTo <= elem['prices'][1] || elem['prices'][1] !== null) {
-               //    console.log(elem);
-               // }
+            } else if (valueFrom != 0) {
+               if (elem['prices'][0] != null) {
+                  if (valueFrom <= elem['prices'][0] || valueFrom <= elem['prices'][1]) {
+                     arrValue.push(elem)
+                  }
+               } else {
+                  if (elem['prices'][1] != null) {
+                     if (valueFrom <= elem['prices'][1]) {
+                        arrValue.push(elem)
+                     }
+                  } else {
+                     arrValue.push(elem)
+                  }
+               }
+            } else if (valueTo != 0) {
+               if (elem['prices'][1] != null) {
+                  if (valueTo >= elem['prices'][1] || valueTo >= elem['prices'][0]) {
+                     arrValue.push(elem)
+                  }
+               } else {
+                  if (elem['prices'][0] != null) {
+                     if (valueTo >= elem['prices'][0]) {
+                        arrValue.push(elem)
+                     }
+                  } else {
+                     arrValue.push(elem)
+                  }
+               }
             }
          })
+         createList(arrValue)
+         arrValue = []
       }
 
       // нажатие кнопк сбросить фильтр
       if (target == clearFilter) {
          clearInput(priceFrom)
          clearInput(priceTo)
+         createList(courses)
       }
    })
 });
